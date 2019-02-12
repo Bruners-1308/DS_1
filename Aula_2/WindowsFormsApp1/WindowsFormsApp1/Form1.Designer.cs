91ae503e877b3ccb82cb91c5176db68c33ab81b8
Bruners-1308 <43767231+Bruners-1308@users.noreply.github.com>
2019-02-12 21:14:28 -0200

    Add files via upload

---
+++ Aula_2/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btCalcula = new System.Windows.Forms.Button();
+            this.txtOperador = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Resultado:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Enabled = false;
+            this.txtResultado.Location = new System.Drawing.Point(15, 25);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(100, 20);
+            this.txtResultado.TabIndex = 1;
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(15, 77);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 20);
+            this.txtNum1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Numero 1:";
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(15, 129);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 20);
+            this.txtNum2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Numero 2:";
+            // 
+            // btCalcula
+            // 
+            this.btCalcula.Location = new System.Drawing.Point(197, 210);
+            this.btCalcula.Name = "btCalcula";
+            this.btCalcula.Size = new System.Drawing.Size(75, 23);
+            this.btCalcula.TabIndex = 6;
+            this.btCalcula.Text = "Calcular";
+            this.btCalcula.UseVisualStyleBackColor = true;
+            this.btCalcula.Click += new System.EventHandler(this.btCalcula_Click);
+            // 
+            // txtOperador
+            // 
+            this.txtOperador.Location = new System.Drawing.Point(15, 176);
+            this.txtOperador.Name = "txtOperador";
+            this.txtOperador.Size = new System.Drawing.Size(100, 20);
+            this.txtOperador.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Operador:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.txtOperador);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btCalcula);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Calculadora::..";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btCalcula;
+        private System.Windows.Forms.TextBox txtOperador;
+        private System.Windows.Forms.Label label4;
+        public System.Windows.Forms.TextBox txtResultado;
+    }
+}
+
---
+++ Aula_2/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btCalcula_Click(object sender, EventArgs e)
+        {
+            int numero1, numero2;
+            string operador;
+            int resultado;
+
+            numero1 = int.Parse(txtNum1.Text);
+            numero2 = Convert.ToInt32(txtNum2.Text);
+
+            if (txtOperador.Text == "+")
+            {
+                resultado = numero1 + numero2;
+            }
+
+            else if (txtOperador.Text == "-")
+            {
+                resultado = numero1 - numero2;
+            }
+
+            else if (txtOperador.Text == "*")
+            {
+                resultado = numero1 * numero2;
+            }
+
+            else if (txtOperador.Text == "/")
+            {
+                resultado = numero1 / numero2;
+            }
+
+            else
+            {
+                resultado = 0;
+            }
+
+
+            txtResultado.Text = resultado.ToString();
+        }
+    }
+}
